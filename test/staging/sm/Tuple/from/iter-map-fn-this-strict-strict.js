21a4b6dfd25310511e95357f47a6e3e3b59d5207
Ms2ger <Ms2ger@igalia.com>
2024-10-25 16:43:40 +0200

    fix

--- test/staging/sm/Tuple/from/iter-map-fn-this-strict-strict.js
+++ test/staging/sm/Tuple/from/iter-map-fn-this-strict-strict.js
@@ -2,9 +2,9 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-includes: [sm/non262-shell.js]
 flags:
-- noStrict
+- onlyStrict
+includes: [sm/non262-shell.js]
 features:
 - Tuple
 description: |
